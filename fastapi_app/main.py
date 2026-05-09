8fc18dcc4731a8aed892d2c4ff878d62ca581ea9
Jackson Cobble <jacksoncobble1234@gmail.com>
2026-05-09 00:46:24 -0500

    removed dagshub authentication
    removed using of dagshub.init, and leaving just mlflow to set the tracking uri of the model.

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -17,13 +17,13 @@ import psycopg
 # Experiment Tracking / Model Registry
 import mlflow
 import mlflow.sklearn
-import dagshub
+# import dagshub
 
 
 load_dotenv()
 
 # set up mlflow dagshub link
-dagshub.init(repo_owner=os.getenv("MLFLOW_TRACKING_USERNAME"), repo_name="ML-Cloud-Deployment", mlflow=True)
+# dagshub.init(repo_owner=os.getenv("MLFLOW_TRACKING_USERNAME"), repo_name="ML-Cloud-Deployment", mlflow=True)
 
 # set the URI and the username / password token will be setup automatically from .env
 mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
